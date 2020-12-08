e20f838bf7c918b66c3c2b8a66499358e6105019
dylvaz <vazquezadylan@gmail.com>
2020-12-08 03:14:48 -0800

    implemnetd mock data to test ui functionality for songList

---
+++ data/albumDetails.ts
@@ -0,0 +1,46 @@
+const albumDetails = {
+  id: '2',
+  name: 'Alfredo',
+  by: 'Freddie Gibbs',
+  likes: '93.k',
+  imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+  artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  songs: [{
+    id: '1',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  },
+  {
+    id: '2',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  },
+  {
+    id: '3',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  },
+  {
+    id: '4',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  },
+  {
+    id: '5',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  },
+  {
+    id: '6',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    title: 'Thuggin',
+    artist: 'Freddie Gibbs',
+  }]
+}
+
+export default albumDetails;
