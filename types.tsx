1eef15df23dc0503c8530f156a7c378eba919e25
dylvaz <vazquezadylan@gmail.com>
2020-12-07 21:10:29 -0800

    implemented type for albums

--- types.tsx
+++ types.tsx
@@ -15,3 +15,9 @@ export type TabOneParamList = {
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
 };
+
+export type AlbumType = {
+  id: string;
+  imgUri: string;
+  artists: string;
+};
