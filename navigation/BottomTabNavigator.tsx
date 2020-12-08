6e11e18578520a2afa3df44b6a0ed29ad7494539
dylvaz <vazquezadylan@gmail.com>
2020-12-08 03:10:15 -0800

    implemented Album Screen in the tabOne nav stack

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+import AlbumScreen from '../screens/AlbumScreen';
 import HomeScreen from '../screens/HomeScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
@@ -50,11 +51,6 @@ export default function BottomTabNavigator() {
   );
 }
 
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
-function TabBarIcon(props: { name: string; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
@@ -67,6 +63,11 @@ function TabOneNavigator() {
         name="Home"
         component={HomeScreen}
         options={{ headerTitle: 'Home' }}
+      />
+        <TabOneStack.Screen
+        name="Album Screen"
+        component={AlbumScreen}
+        options={{ headerTitle: 'Album Screen' }}
       />
     </TabOneStack.Navigator>
   );
