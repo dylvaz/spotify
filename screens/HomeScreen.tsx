c8d64ec267c5327e8ae8ede3607e76e53317e0cf
dylvaz <vazquezadylan@gmail.com>
2020-12-07 21:07:20 -0800

    implemented view for home page with a flat list that displays a vertical list of album rows

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import albumRowList from '../data/albumRowLists';
+import AlbumRow from '../components/albumRow';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={albumRowList}
+        renderItem={({ item }) => <AlbumRow title={item.title} albums={item.albums}/>}
+        keyExtractor={( item ) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
