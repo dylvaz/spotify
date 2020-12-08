77224ac8a56a7560b773b63c1f2a8a195e4ee351
dylvaz <vazquezadylan@gmail.com>
2020-12-07 21:09:40 -0800

    implemented mockdata for implementation of front end

---
+++ data/albumRowLists.ts
@@ -0,0 +1,74 @@
+const albumRowList =[{
+  id: '1',
+  title: 'Bangers',
+  albums: [{
+    id: '1',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '2',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '3',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '4',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  }]
+},
+{
+  id: '2',
+  title: 'Bangers',
+  albums: [{
+    id: '5',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '6',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '7',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '8',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  }]
+},
+{
+  id: '3',
+  title: 'Bangers',
+  albums: [{
+    id: '9',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '10',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '11',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  },
+  {
+    id: '12',
+    imgUri: 'https://i.pinimg.com/originals/4c/8e/62/4c8e62024346ece520c2dec0bc9e1083.png',
+    artists: 'MF Doom, J. Cole, Freddie Gibbs',
+  }]
+}]
+
+export default albumRowList;
