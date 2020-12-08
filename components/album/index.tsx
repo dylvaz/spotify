26585263239825e3c74e99ea98ac52abf94de52e
dylvaz <vazquezadylan@gmail.com>
2020-12-08 03:08:05 -0800

    implemented onPress functionality

--- components/album/index.tsx
+++ components/album/index.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 import styles from './styles';
 import { AlbumType } from '../../types';
+import { useNavigation } from '@react-navigation/native';
 
 export type AlbumProps = {
   album: AlbumType,
-}
+};
 
 const Album = (props: AlbumProps) => {
+
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate('Album Screen', { id: props.album.id });
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: props.album.imgUri }} style={styles.image}/>
-      <Text style={styles.text}>{props.album.artists}</Text>
-    </View>
-  )
-}
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <Image source={{ uri: props.album.imgUri }} style={styles.image} />
+        <Text style={styles.text}>{props.album.artists}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 export default Album;
