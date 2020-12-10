7bc61d467465dcb39b5f45888310401d802a6d1a
dylvaz <vazquezadylan@gmail.com>
2020-12-10 02:37:18 -0800

    fixed formatting

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react';
 import SongListItem from '../components/songListItem';
 import { useRoute } from '@react-navigation/native';
 import { View, Text, FlatList } from 'react-native';
-import AlbumHeader from '../components/albumScreenHeader'
+import AlbumHeader from '../components/albumScreenHeader';
 import albumDetails from '../data/albumDetails';
 
 const AlbumScreen = () => {
   const route = useRoute();
 
   useEffect(() => {
-
+    
   }, []);
 
   return (
